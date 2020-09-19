c65fe9c2c60a84f79d4f101b4eb446ad2b0eb6d1
Harue Lico <44547634+haruelico@users.noreply.github.com>
2020-09-20 03:34:35 +0900

    Load toml configure

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/haruelico/safeprintenv
 
 go 1.13
+
+require github.com/pelletier/go-toml v1.8.1
--- main.go
+++ main.go
@@ -2,9 +2,21 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
+	"path"
 	"strings"
+
+	"github.com/pelletier/go-toml"
 )
+
+type Configure struct {
+	SensitiveList SensitiveList
+}
+
+type SensitiveList struct {
+	Keys []string
+}
 
 func main() {
 	envs := os.Environ()
@@ -12,6 +24,11 @@ func main() {
 	for _, env := range envs {
 		fmt.Println(isSnsitiveEnvVar(env))
 	}
+	conf, err := loadConfigure()
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(conf)
 }
 
 func isSnsitiveEnvVar(env string) bool {
@@ -20,4 +37,26 @@ func isSnsitiveEnvVar(env string) bool {
 		return true
 	}
 	return false
+}
+
+func loadConfigure() (*Configure, error) {
+	conf := new(Configure)
+	// confDir, err := os.UserConfigDir()
+
+	// if err != nil {
+	// 	return nil, err
+	// }
+
+	// confPath := path.Join(confDir, "safeprintenv", "config.toml")
+	confPath := path.Join("config.example.toml")
+	if _, err := os.Stat(confPath); os.IsNotExist(err) {
+		// config file not exist. Return empty config
+		return conf, nil
+	}
+
+	// rawConfBody, err := ioutil.ReadFile(path.Join(confPath))
+	rawConfBody, _ := ioutil.ReadFile("config.example.toml")
+	toml.Unmarshal(rawConfBody, conf)
+
+	return conf, nil
 }
