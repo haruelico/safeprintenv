194772f8011f7b6399607a487b5c8e2094ed5003
Harue Lico <44547634+haruelico@users.noreply.github.com>
2020-09-19 17:06:50 +0900

    Imprement very very simple feature

--- main.go
+++ main.go
@@ -3,12 +3,20 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
 	envs := os.Environ()
 
 	for _, env := range envs {
-		fmt.Println(env)
+		fmt.Println(isSnsitiveEnvVar(env))
 	}
+}
+
+func isSnsitiveEnvVar(env string) bool {
+	if strings.HasPrefix(env, "SAMPLE_SECRET_ENV") {
+		return true
+	}
+	return false
 }
